e03bd7edff430ba35b28c4911b0fe570ec94f894
Paul.Sikhakhane <paulnhlanhlasikhakhane@gmail.com>
2026-08-20 00:24:37 +0200

    Switched to cloudamqp.com
    Local host was not working due to an corrupted installation of Erlang and Docker.so now I'm using a cloud service to act as my RabbitMQ

--- stock_producer.py
+++ stock_producer.py
@@ -1,7 +1,7 @@
 import pika
 import json
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.URLParameters('amqps://tviucipo:RGXbxLb3Hg2quA9IOBe_TOSbPP3xd51A@gerbil.rmq.cloudamqp.com/tviucipo'))
 channel = connection.channel()
 
 channel.queue_declare(queue='stock_updates')
@@ -10,7 +10,7 @@ update = {"product_id": "SKU-1001", "new_quantity": 42}
 
 channel.basic_publish(
     exchange='',
-    routing_='stock_updates',
+    routing_key='stock_updates',
     body=json.dumps(update)
 )
 print(f" [x] Sent stock updates: {update}")
