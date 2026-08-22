7eeffe3a629a2de9aae140b715f20587302c063b
Paul.Sikhakhane <paulnhlanhlasikhakhane@gmail.com>
2026-08-22 13:31:37 +0200

    connection change
    local to url

--- receive.py
+++ receive.py
@@ -1,6 +1,6 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.URLParameters('amqps://tviucipo:RGXbxLb3Hg2quA9IOBe_TOSbPP3xd51A@gerbil.rmq.cloudamqp.com/tviucipo'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
